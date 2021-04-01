39029990f71064aef452aa151559c382b646b54c
CasasolaCano <casasolacontact@gmail.com>
2021-04-01 13:54:45 +0200

    comments bad practice

--- src/exercices/01-basic-types.ts
+++ src/exercices/01-basic-types.ts
@@ -2,6 +2,19 @@ let nameMajor: string = 'Strider';
 let hp: number = 95;
 let isAlive: boolean = true;
 
+/* 
+    Malas prácticas de typescript, se puede definir una variable como una variable
+    que puede almacenar dos tipos de datos, por ejemplo de tipo número o de tipo string,
+    esto es una mala práctica, dado que typescript nos incita a tener un tipado estricto
+    deberíamos mantener las variables lo mas detalladas posibles para evitar errores
+    de tipado.
+*/
+let hpCharacter: number | string = 85;
+
+hpCharacter = 'FULL';
+
+
+
 nameMajor = 'Fernando';
 
 console.log(isAlive);
