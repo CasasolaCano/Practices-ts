fced2aa91bf9c0acf9e8f307b49c7ec80ee92005
CasasolaCano <casasolacontact@gmail.com>
2021-04-11 13:24:17 +0200

    Example of destructuring arguments

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,35 @@
 
-interface Player {
 
-    volume: number;
-    second: number;
-    song: string;
-    details:Details;
+interface Product {
+    desc: string;
+    price: number;
 }
 
-interface Details {
-    author: string;
-    year: number;
+const telephone: Product = {
+    desc: 'Nokia A1',
+    price: 150
 }
 
-const player: Player = {
-    volume: 90,
-    second: 30,
-    song: 'Mess around',
-    details: {
-        author: 'Cage the Elephant',
-        year: 2015
-    }
+const tablet: Product = {
+    desc: 'iPad Air',
+    price: 350
 }
 
-/*
-    Podemos desestructurar los objetos, esto nos deja un código mas limpio y ordenado,
-    tenemos varias maneras de desestructurar un objeto dentro de otro,
-    el siguiente ejemplo puede ser algo complicado de entender cuando tenemos mucho
-    código escrito.
-*/
+function calculateISV(products : Product[]): number[] {
 
-const {volume, second, song, details:{author} } = player;
+    let total = 0;
 
-//Este ejemplo comentado aqui abajo sería la manera correcta para mantener el código lo mas limpio posible.
+    products.forEach( ({ price }) => {
 
-// const { author } = details;
+        total += price
+    })
 
-console.log('El volumen actual es de: ', volume);
-console.log('El segundo actual es de: ', second);
-console.log('La cancion actual es de: ', song);
-console.log('El autor actual es: ', author);
+    return [total,total * 0.21];
+}
 
+const items = [ telephone, tablet];
 
+const [total, isv] = calculateISV(items);
 
-
-
-
-//Desestructuración de arrays
-
-const rockAlbums: string[] = ['OK Computer', 'In the court of the Crimson King', 'Close To The Edge'];
-
-
-/*
-    En la desestructuración de arrays lo importante es la posición del nombre.
-    Se le ponen nombres a las constantes basadas en su posición.
-*/
-
-
-const [album1, album2, album3 ] = rockAlbums;
-
-// Si no queremos desestructurar las primeras posiciones las tenemos que escribir vacias como en el siguiente ejemplo.
-// Esta desestructuración de arrays saltando algunas posiciones no suele ser común pero puede hacerse.
-
-// const [ , , album3] = rockAlbums;
-
-console.log('Album 1: ', album1);
-console.log('Album 2: ', album2);
-console.log('Album 3: ', album3);
+console.log('Total: ', total );
+console.log('ISV: ', isv);
